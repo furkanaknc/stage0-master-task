b49e6c7aa68c84714e59f0bd6922743cdc3b10a7
U210709060 <furkanakinci4@posta.mu.edu.tr>
2023-07-13 13:59:15 +0300

    OPP-tasks-has-done

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,12 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends com.epam.OOP.Animal {
+    public Bird(){
+        super("blue",2,false);
+    }
 
+    public String getDescription(){
+        String wingsStatus = "it has 2 wings and can fly.";
+        return super.getDescription() + " Moreover, " + wingsStatus;
+    }
 }
